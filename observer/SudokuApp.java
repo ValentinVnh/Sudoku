1e6468b595d484c38496544d7571b05074858446
ValentinVnh <92478691+ValentinVnh@users.noreply.github.com>
2023-03-30 00:01:59 +0200

    Implementation du controller v0.1

--- observer/SudokuApp.java
+++ observer/SudokuApp.java
@@ -1,17 +1,13 @@
 package observer;
 
+import observer.controller.ControllerSudoku;
 import observer.modele.ModelSudoku;
 import observer.observer.CaseObserver;
 import observer.observer.SudokuObserver;
 
 public class SudokuApp {
     public static void main(String[] args) {
-
-        SudokuObserver observer = new CaseObserver();
-        ModelSudoku sudoku = new ModelSudoku("sudoku1.txt");
-        SudokuSolver solver = new SudokuSolver();
-
-        sudoku.addObserver(observer);
-        solver.solve(sudoku);
+        ControllerSudoku controller = new ControllerSudoku("sudoku1.txt");
+        controller.solve();
     }
 }
